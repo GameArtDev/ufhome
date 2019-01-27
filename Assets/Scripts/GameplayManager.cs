ffc3f49615e7d491b9921c62b2420ac66bd47609
carnefrisca <giovanni.coli@gmail.com>
2019-01-27 10:04:01 +0100

    NON TOCCO PIù IL MASTER

---
+++ Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayManager : MonoBehaviour
+{
+    public GameObject ufo;
+    List<GameObject> ufos = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        //for (int i=0; i< 3; i++)
+        //{
+        //    MovesManager moves =  ufo.GetComponent(typeof(MovesManager)) as MovesManager;
+        //    moves.startPosition = new Vector3(ufo.transform.position.x + i, ufo.transform.position.y + i, 0);
+
+        //    Instantiate(ufo);
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (GameObject.FindGameObjectsWithTag("ufo").Length < 1)
+        {
+            MovesManager moves = ufo.GetComponent(typeof(MovesManager)) as MovesManager;
+            moves.startPosition = new Vector3(ufo.transform.position.x + 1, ufo.transform.position.y + 1, 0);
+            ufo.tag = "ufo";
+            Instantiate(ufo);
+
+        }
+    }
+}
--- Assets/Scripts/MovesManager.cs
+++ Assets/Scripts/MovesManager.cs
@@ -12,12 +12,18 @@ public class MovesManager : MonoBehaviour
 
     public Vector2 speed = new Vector2(10, 10);
     public Vector2 direction = new Vector2(-1, 0);
+    public Vector3 startPosition;
+
+    public void Start()
+    {
+        
+    }
 
     public void Update()
     {
         index += Time.deltaTime;
-        float x = speed.x * direction.x; //amplitudeX * Mathf.Cos(omegaX * index);
-        float y = amplitudeY * Mathf.Sin(omegaY * index);
+        float x = startPosition.x + (amplitudeX * Mathf.Cos(omegaX * index));
+        float y = startPosition.y + Mathf.Abs(amplitudeY * Mathf.Sin(omegaY * index));
         transform.localPosition = new Vector3(x, y, 0);
     }
 
