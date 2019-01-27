92ea477b0a7a67e8b702458e52ee9e6fc8665a12
carnefrisca <giovanni.coli@gmail.com>
2019-01-27 10:53:01 +0100

    ufo spawn

---
+++ Assets/Scripts/ContenxtInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class ContenxtInfo
+{
+
+    private static float leftBorder;
+    private static float rightBorder;
+    private static float topBorder;
+    private static float bottomBorder;
+
+    public static float LeftBorder
+    {
+        get { return leftBorder; }
+        set { leftBorder = value; }
+    }
+
+    public static float RightBorder
+    {
+        get { return rightBorder; }
+        set { rightBorder = value; }
+    }
+
+    public static float TopBorder
+    {
+        get { return topBorder; }
+        set { topBorder = value; }
+    }
+
+    public static float BottomBorder
+    {
+        get { return bottomBorder; }
+        set { bottomBorder = value; }
+    }
+
+}
+
--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -6,32 +6,39 @@ public class GameplayManager : MonoBehaviour
 {
     public GameObject ufo;
     List<GameObject> ufos = new List<GameObject>();
-
+    public float numberOfUfos = 1;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
-        //for (int i=0; i< 3; i++)
-        //{
-        //    MovesManager moves =  ufo.GetComponent(typeof(MovesManager)) as MovesManager;
-        //    moves.startPosition = new Vector3(ufo.transform.position.x + i, ufo.transform.position.y + i, 0);
-
-        //    Instantiate(ufo);
-        //}
+        var dist = (transform.position - Camera.main.transform.position).z;
+        ContenxtInfo.LeftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).x;
+        ContenxtInfo.RightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, dist)).x;
+        ContenxtInfo.TopBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).y;
+        ContenxtInfo.BottomBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, dist)).y;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (GameObject.FindGameObjectsWithTag("ufo").Length == 0)
+        {
+            GenerateUfo(numberOfUfos);
+        }
+    }
 
-
-        if (GameObject.FindGameObjectsWithTag("ufo").Length < 1)
+    void GenerateUfo(float numOfUfo)
+    {
+        for (int i = 0; i < numberOfUfos; i++)
         {
             MovesManager moves = ufo.GetComponent(typeof(MovesManager)) as MovesManager;
-            moves.startPosition = new Vector3(ufo.transform.position.x + 1, ufo.transform.position.y + 1, 0);
+            moves.startPosition = new Vector3(
+                Random.Range(ContenxtInfo.LeftBorder, ContenxtInfo.RightBorder),
+                Random.Range(-5.4f, -4.0f)
+                , 0);
             ufo.tag = "ufo";
+            ufo.transform.rotation = new Quaternion(0, 0, 0, 0);
             Instantiate(ufo);
-
         }
     }
 }
